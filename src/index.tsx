a5961dc7d32e196865c86a3a210e93cb64c6e2b8
PavelM <myagkiypavel@gmail.com>
2023-07-03 15:53:42 +0300

    chore: App

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme/theme";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
