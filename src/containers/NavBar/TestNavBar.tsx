44ca40fc60d3bc8417ed218463b0fff742459ea2
PavelM <myagkiypavel@gmail.com>
2023-07-04 11:02:05 +0300

    feature: add TestNavBar

---
+++ src/containers/NavBar/TestNavBar.tsx
@@ -0,0 +1,10 @@
+import { AppBar, Typography } from '@mui/material'
+import React from 'react'
+
+export const TestNavBar = () => {
+  return (
+    <AppBar color='primary' position='static'>
+      <Typography> App Bar</Typography>
+    </AppBar>
+  )
+}
