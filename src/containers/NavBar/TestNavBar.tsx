bdfab5f20c03bb8c85ab8cc5c609f119f22db5c9
PavelM <myagkiypavel@gmail.com>
2023-07-04 11:29:11 +0300

    chore: add Container to TestNavBar

--- src/containers/NavBar/TestNavBar.tsx
+++ src/containers/NavBar/TestNavBar.tsx
@@ -1,10 +1,25 @@
-import { AppBar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Container, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import React from "react";
 
 export const TestNavBar = () => {
   return (
-    <AppBar color='primary' position='static'>
-      <Typography> App Bar</Typography>
+    <AppBar color="primary" position="static">
+      <Container
+        sx={{
+          "@media (min-width:360px)": {
+            backgroundColor: "green",
+            padding: "18px 0 22px",
+          },
+          "@media (min-width:1973px)": {
+            backgroundColor: "red",
+            maxWidth: "1288px",
+            padding: "19px 0 17px",
+          },
+        }}
+      >
+        <Typography> App Bar</Typography>
+      </Container>
     </AppBar>
-  )
-}
+  );
+};
