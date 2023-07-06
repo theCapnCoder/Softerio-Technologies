7ea20f37b9e38ee8bb2b14ff1735b753ad07c93d
PavelM <myagkiypavel@gmail.com>
2023-07-06 20:39:50 +0300

    chore: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Box, Container, Typography, ContainerProps } from "@mui/material";
+import { NavBar, TestNavBar } from "./containers/NavBar";
 
-const containerStyle: ContainerProps['sx'] = {
-  backgroundColor: '#FEF4FF'
-}
+const containerStyle: ContainerProps["sx"] = {
+  backgroundColor: "#FEF4FF",
+};
 
 function App() {
   return (
-    <Container sx={containerStyle}>
-      <Typography variant="h1" component={"h1"}>
+    <Container disableGutters sx={containerStyle} maxWidth="xl" >
+      {/* <NavBar /> */}
+      <TestNavBar />
+      {/* <Typography variant="h1" component={"h1"}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
       </Typography>
-      <Typography variant="h2" component={"h1"} style={{ fontWeight: 600}}>
+      <Typography variant="h2" component={"h1"} style={{ fontWeight: 600 }}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
       </Typography>
       <Typography variant="h2" component={"h1"}>
@@ -24,7 +27,7 @@ function App() {
       </Typography>
       <Typography variant="h5" component={"h1"}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
-      </Typography>
+      </Typography> */}
     </Container>
   );
 }
