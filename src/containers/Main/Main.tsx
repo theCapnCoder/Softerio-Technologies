c96a677e7741b4fd917c4b10aa04197eb5a7c2af
PavelM <myagkiypavel@gmail.com>
2023-07-10 19:06:45 +0300

    style: text and icons in Main

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -2,35 +2,44 @@ import { Box, BoxProps, Button, Stack, Typography } from "@mui/material";
 import { Cup, Rocket } from "../../assets/icons";
 
 const containerStyle: BoxProps["sx"] = {
+  mx: 0.5,
   pt: 5,
   borderRadius: 2.5,
   backgroundColor: "#3980FF",
 };
 
 const iconStyles: BoxProps["sx"] = {
-  // mr: 2.1,
-  maxWidth: 36,
+  width: 36,
   height: 36,
 };
 
 export const Main = () => {
   return (
     <Box sx={containerStyle}>
-      <Typography variant="h1">
+      <Typography variant="h1" sx={{ m: "0 auto", mb: 3.37, maxWidth: 257 }}>
         Разработка мобильных приложений и сервисов
       </Typography>
 
       <Box>
-        <Stack direction={"row"}>
+        <Stack
+          mb={1.4}
+          direction={"row"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
           <Rocket sx={iconStyles} />
-          <Typography variant="textWhite">
+          <Typography variant="mainText" sx={{ ml: 2.1, maxWidth: 248 }}>
             Знаем, как решить вашу задачу оптимальным способом
           </Typography>
         </Stack>
 
-        <Stack direction={"row"}>
+        <Stack
+          direction={"row"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
           <Cup sx={iconStyles} />
-          <Typography variant="textWhite">
+          <Typography variant="mainText" sx={{ ml: 2.1, maxWidth: 248 }}>
             Подберем подходящее решение с учетом целей и возможностей
           </Typography>
         </Stack>
