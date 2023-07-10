bb005ee394c85f3b347ff49256000165ab1f8278
PavelM <myagkiypavel@gmail.com>
2023-07-10 18:50:50 +0300

    chore: Main add Typogrhaphy

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Box, BoxProps, Button, Stack, Typography } from "@mui/material";
+import { Cup, Rocket } from "../../assets/icons";
+
+const containerStyle: BoxProps["sx"] = {
+  pt: 5,
+  borderRadius: 2.5,
+  backgroundColor: "#3980FF",
+};
+
+const iconStyles: BoxProps["sx"] = {
+  // mr: 2.1,
+  maxWidth: 36,
+  height: 36,
+};
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <Box sx={containerStyle}>
+      <Typography variant="h1">
+        Разработка мобильных приложений и сервисов
+      </Typography>
+
+      <Box>
+        <Stack direction={"row"}>
+          <Rocket sx={iconStyles} />
+          <Typography variant="textWhite">
+            Знаем, как решить вашу задачу оптимальным способом
+          </Typography>
+        </Stack>
+
+        <Stack direction={"row"}>
+          <Cup sx={iconStyles} />
+          <Typography variant="textWhite">
+            Подберем подходящее решение с учетом целей и возможностей
+          </Typography>
+        </Stack>
+      </Box>
+
+      <Button>Заказать приложение</Button>
+      <Button>Онлайн-консультация</Button>
+    </Box>
+  );
+};
